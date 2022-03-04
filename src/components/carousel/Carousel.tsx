b23319da2e343413ed7e2bbbcb4b530be9994a3e
Attila Werlein <attila.werlein@voiapp.io>
2022-03-04 12:04:00 +0100

    add styling to carousel
    - add arrow
    - fix positioning
    - show one image at the time
    - use flex

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,12 +1,47 @@
+import styled from 'styled-components'
+
 interface Props {
     imageURLs: string[]
 }
 
+const width = 500;
+const height = 500;
+
 export function Carousel(props: Props) {
-    
+
     return (
-        <div>
-            {props.imageURLs.map((imageUrl) => <img key={imageUrl} src={imageUrl} alt="" />)}
-        </div>
+        <CarouselContainer>
+            <div><Arrow direction='left'/></div>
+            <ImagesContainer>
+                {props.imageURLs.map((imageUrl, index) => <Image left={index * 500} key={imageUrl} src={imageUrl} alt="" />)}
+            </ImagesContainer>
+            <div><Arrow direction='right'/></div>
+        </CarouselContainer>
     )
 }
+
+const CarouselContainer = styled.div`
+    display: flex;
+`
+
+const ImagesContainer = styled.div`
+    position: relative;
+    background: red;
+    width: ${width}px;
+    height: ${height}px;
+    overflow: hidden;
+`
+const Image = styled.img<{ left: number }>`
+    position: absolute;
+    max-width: ${width}px;
+    max-height: ${height}px;
+    left: ${props => props.left}px;
+`
+
+const Arrow = styled.i<{ direction: 'left' | 'right' }>`
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    transform: rotate(${props => props.direction === 'left' ? '135' : '-45'}deg);
+`
