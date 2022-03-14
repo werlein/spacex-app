fe646bf2f1e96cac7ba46e102dcdc0907c43de16
Attila Werlein <attila.werlein@voiapp.io>
2022-03-14 14:43:53 +0100

    Add access to data for single Rocket
    - fetch rockets data and access the redux store from Rocket.tsx
    - feed the Carousel with the right flickr_images

--- src/components/rocket/Rocket.tsx
+++ src/components/rocket/Rocket.tsx
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "src/redux/hooks";
+import { rocketActions } from "src/redux/rocket/rocket.actions";
+import { selectRockets } from "src/redux/rocket/rocket.reducer";
 import { Carousel } from "../carousel/Carousel";
 
 export function Rocket() {
+    const { id } = useParams();
+    const dispatch = useAppDispatch();
+    
+    useEffect(() => {
+        dispatch(rocketActions.getRockets())
+    }, [dispatch])
+
+    const rockets = useAppSelector(selectRockets);
+    const rocket = rockets.find(rocket => rocket.id === id);
+
+/**
+ * Add new api to fetch one rocket by id 
+ * 
+ * - URL : https://api.spacexdata.com/v4/rockets/:id
+ * - Docs: https://github.com/r-spacex/SpaceX-API/blob/master/docs/rockets/v4/one.md
+ * 
+ * 
+ */
+    
     return (
         <>
             <div>
-                fooBar
-                <Carousel imageURLs={[]} />
+                {rocket && (
+                    <>
+                        {rocket.name}
+                        <Carousel imageURLs={rocket.flickr_images} />
+                    </>
+                )}
             </div>
         </>
     )
