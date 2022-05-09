12dcd46526ff5a9c596d5f955b18d1e07047b2ca
Danny Skoog <danny.skoog@live.se>
2022-05-09 16:38:41 +0200

    refactor(search): add proper generic type constraints (#1)

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,33 +1,42 @@
 import Input from "@mui/material/Input";
 import { ChangeEvent, useEffect, useState } from "react";
 
-interface Props<T> {
+interface Props<T, K> {
     data: T[],
-    keys: (keyof T)[],
+    keys: K[],
     onSearch: (results: T[]) => void
 }
 
-// ToDo: fix the Generic Typings and the Placeholder in the input field
+// ToDo: fix the Placeholder in the input field
 
-export function Search<T>({ data, keys, onSearch }: Props<T>) {
+type StringProps<T> = { [K in keyof T]: T[K] extends string ? K : never }[keyof T];
+
+export function Search<T extends Record<K, string>, K extends StringProps<T>>({ data, keys, onSearch }: Props<T, K>) {
     const [query, setQuery] = useState<string>()
 
     useEffect(() => {
         if (query !== undefined) {
+            if (query?.trim().length === 0) {
+                onSearch(data)
+                return
+            }
+
             let results: T[] = []
-            if (query.trim().length === 0) {
-                results = data
-            } else {
-                for (const key of keys) {
-                    for (const item of data) {
-                        const value = item[key] as unknown as string
-                        if (value.toLowerCase().includes(query.toLowerCase())) {
-                            results.push(item)
-                        }
+
+            for (const key of keys) {
+                for (const item of data) {
+                    if (results.includes(item)) {
+                        continue
+                    }
+
+                    const value = item[key]
+                    if (value.toLowerCase().includes(query.toLowerCase())) {
+                        results.push(item)
                     }
                 }
             }
-            onSearch([...new Set(results)])
+
+            onSearch(results)
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [query])
